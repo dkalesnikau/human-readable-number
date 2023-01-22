4762beedaae35b0f629a3ba72a7051707f556434
Dzmitry Kalesnikau <chem@chem.by>
2023-01-23 00:20:20 +0300

    fix: code

--- src/index.js
+++ src/index.js
@@ -1,21 +1,28 @@
 let unitsArray = ['zero','one','two','three','four','five','six','seven','eight','nine'];
 let secArray = ['ten', 'eleven', 'twelve','thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
-let nextArray = ['ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+let lastArray = ['ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
 
-const getUnits = (num) => { return unitsArray[num]; }
+const getUnits = (num) => {   
+    return unitsArray[num];
+}
 
 const getDozens = (num) => {
     if (num < 10) {
         return getUnits(num);
     } else if (num < 20){
       return secArray[num - 10];
-  } else { return `${nextArray[Math.floor(num/10)-1]} ${(num%10) == 0 ? '':' '+ getUnits(num%10)}`; }
+  } else {
+      return `${lastArray[Math.floor(num/10)-1]}${(num%10) == 0 ? '':' '+ getUnits(num%10)}`;
+  }
 }
 
 module.exports = function toReadable (number) {
+
 if (number<10){
     return getUnits(number);
     }else if (number < 100){
         return getDozens(number);
-        }else{ return `${getUnits(number.toString()[0])} hundred ${number%100 == 0 ? '' : ' '+ getDozens(number%100)}`; };
+        }else{
+          return `${getUnits(number.toString()[0])} hundred${number%100 == 0 ? '' : ' '+ getDozens(number%100)}`;
+  };
 };
